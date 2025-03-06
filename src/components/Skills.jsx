e1639f4744b7237e18632bb3f823f3da90b74aa2
jaswanth Sai <jasvanthsai7@gmail.com>
2025-03-06 10:46:41 +0530

    aupdated the skill secction with header

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,19 @@
 "use client";
 
 import { ParallaxText } from "./ui/ParallaxText";
-import SectionHeader from "./SectionHeader";
-
+import { SectionHeading } from "./ui/Typography";
+import { SlideIn } from "./ui/Transitions";
 
 function Skills({ skills }) {
   return (
     <section id="skills">
-      <div className="md:p-8 p-4">
+      {/* <div className="md:p-8 p-4">
         <SectionHeader title={"Skills"} />
-      </div>
+      </div> */}
+      <SectionHeading className="pl-4 md:px-12 py-20">
+        <SlideIn className="text-white/40">SKILLS</SlideIn>
+
+      </SectionHeading>
       <ParallaxText baseVelocity={-5}>
         {skills.map((skill) =>
           skill.enabled ? (
