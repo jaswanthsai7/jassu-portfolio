89acbe9b3ad4b6e11a0a0769c30939fc7de0c64b
jaswanth Sai <jasvanthsai7@gmail.com>
2025-03-07 10:35:34 +0530

    fixed the module issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Header from "@/components/Header";
-import Hero from "@/components/hero";
-import Projects from "@/components/projects";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Timeline from "@/components/Timeline";
 import { Transition } from "@/components/ui/Transitions";
